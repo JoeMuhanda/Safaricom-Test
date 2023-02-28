48012a739f9b522510dfb66f3696626436fff0a5
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-03-01 00:52:12 +0300

    Create soln.py

---
+++ soln.py
@@ -0,0 +1,20 @@
+def solution(A):
+    # Filter out negative integers and 0
+    A = list(filter(lambda x: x > 0, A))
+    
+    # If the filtered list is empty, the smallest positive integer is 1
+    if not A:
+        return 1
+    
+    # Sort the list and find the smallest positive integer
+    A.sort()
+    smallest_int = 1
+    for i in A:
+        if i == smallest_int:
+            smallest_int += 1
+        elif i > smallest_int:
+            return smallest_int
+    
+    # If all integers in the list are consecutive positive integers, 
+    # the smallest missing positive integer is the next positive integer after the last element
+    return A[-1] + 1
