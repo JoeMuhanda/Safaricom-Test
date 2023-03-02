e3e4c624328a7e346066f48bafaa17e9346cabce
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-03-02 22:32:27 +0300

    Update soln.py

--- soln.py
+++ soln.py
@@ -16,3 +16,6 @@ def solution(A):
             return smallest_int
     
    
+ # If all integers in the list are consecutive positive integers, 
+    # the smallest missing positive integer is the next positive integer after the last element
+    return A[-1] + 1
